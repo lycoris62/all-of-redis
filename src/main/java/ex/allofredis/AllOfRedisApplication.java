816673e73e1687e95cc61240d4318c25bb2c55c4
barami62 <wodbs7758@naver.com>
2024-03-05 12:16:51 +0900

    first commit

---
+++ src/main/java/ex/allofredis/AllOfRedisApplication.java
@@ -0,0 +1,12 @@
+package ex.allofredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllOfRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllOfRedisApplication.class, args);
+    }
+}
