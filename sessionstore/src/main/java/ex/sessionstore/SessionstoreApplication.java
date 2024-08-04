534bb45c94af6b3d0e6d3b4971a9e7c8d3130d88
barami62 <wodbs7758@naver.com>
2024-08-04 18:01:30 +0900

    레디스 세션 저장소 프로젝트 시작

---
+++ sessionstore/src/main/java/ex/sessionstore/SessionstoreApplication.java
@@ -0,0 +1,12 @@
+package ex.sessionstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionstoreApplication.class, args);
+    }
+}
