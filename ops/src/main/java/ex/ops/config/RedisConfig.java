21445774ebf4207a6c45b55b3007334ffd8aee18
barami62 <wodbs7758@naver.com>
2024-08-06 14:28:33 +0900

    Feat(serializer): 시리얼라이저 설정 추가

---
+++ ops/src/main/java/ex/ops/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package ex.ops.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Item> template(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<String, Item> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+
+        return redisTemplate;
+    }
+}
